43eddd433cb43524d07f303035fb2239b803b694
mobius29 <sagjin0000@naver.com>
2024-11-09 21:10:57 +0900

    feat: style cursor navbar

--- src/components/common/Layout/NavBar.tsx
+++ src/components/common/Layout/NavBar.tsx
@@ -1,15 +1,17 @@
 import { CalendarOutlined, SearchOutlined, StockOutlined, UserOutlined } from '@ant-design/icons';
 import { Flex } from 'antd';
 
+const ICON_CLASS = 'flex justify-center items-center w-full cursor-pointer';
+
 const NavBar = () => {
   return (
     <Flex
       justify='space-around'
       className='fixed bottom-0 max-w-[476px] w-full h-[40px] text-2xl rounded-t-lg border-t-2 shadow-[0_-5px_10px_0_rgba(0,0,0,0.1)] bg-white'>
-      <StockOutlined className='flex justify-center items-center w-full' />
-      <CalendarOutlined className='flex justify-center items-center w-full' />
-      <SearchOutlined className='flex justify-center items-center w-full' />
-      <UserOutlined className='flex justify-center items-center w-full' />
+      <StockOutlined className={ICON_CLASS} />
+      <CalendarOutlined className={ICON_CLASS} />
+      <SearchOutlined className={ICON_CLASS} />
+      <UserOutlined className={ICON_CLASS} />
     </Flex>
   );
 };
